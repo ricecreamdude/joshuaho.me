ec11b0937ee034d13dc3fb3c7f53126158770d37
Joshua Ho <ho.joshua4@gmail.com>
2020-08-20 21:29:54 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container } from '@material-ui/core';
+
+import Home from './views/Home';
+
+function App() {
+ 
+  return (
+    <Container>
+      <Home />
+      
+    </Container>
+  );
+
+}
+
+export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home(){
+
+    return(
+        <div>
+            <h1>HI I AM HOME PAGE</h1>
+        </div>
+    )
+
+};
+
+export default Home;
