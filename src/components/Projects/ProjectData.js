258abf056c917720371996489eb3aaf23c0f52b4
Josh Ho <josh.ho@slalom.com>
2020-10-19 20:35:56 -0700

    Added projects and meta descriptions

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -1,24 +1,59 @@
 const projectData = [
     {
+        companyTitle:"Cleareview",
+        cardImage: "./images/portfolio/cleareview/cleareview-logo.png",
+        techUsed:"Vue, Vuetify, VueX, API",
+        modalSubtitle:"Eliminate bias in radiology litigation.",
+        modalDescription:"Worked with a senior engineer to deliver various API-integrated components using the component library Vuetify. ",
+        modalImages:[],
+        modalImageTemp:'./images/portfolio/dermarts/dermatology_arts_modal.jpg',
+        tags:[],
+    },
+    {
+        companyTitle:"China Med Device",
+        cardImage: "./images/portfolio/chinameddevice/cmd-modal.jpg",
+        techUsed:"HTML/CSS, WordPress",
+        url:"https://chinameddevice.com/",
+        modalImages:[],
+        modalImageTemp:'./images/portfolio/chinameddevice/cmd-modal.jpg',
+        modalSubtitle:"Provides regulatory intelligence to help companies access China’s fast-growing $80-billion MedTech market.",
+        modalDescription:"While working under Explore, recreated the client's personal business page. Using WordPress and HTML/CSS.",
+        tags:[],
+    },
+    {
         companyTitle:"Dermatology Arts",
         cardImage: "./images/portfolio/dermarts/logo.png",
-        techUsed:"HTML/CSS",
+        techUsed:"HTML/CSS, Squarespace",
         url:"https://dermatologyarts.com/",
         modalImages:[],
         modalImageTemp:'./images/portfolio/dermarts/dermatology_arts_modal.jpg',
         modalSubtitle:"Bellevue’s Top Dermatologists",
-        modalDescription:"A friendly, caring, modern dermatology practice focused on real medical and surgical skin needs. Uses HTML/CSS and SquareSpace.",
+        modalDescription:"A friendly, caring, modern dermatology practice focused on real medical and surgical skin needs. Worked on site with client to consult on design. Uses HTML/CSS and SquareSpace.",
         tags:[],
     },
     {
-        companyTitle:"Cleareview",
-        cardImage: "./images/portfolio/dermarts/logo.png",
-        techUsed:"HTML/CSS",
-        modalDescription:"",
-        modalSubtitle:"",
+        companyTitle:"Dr Bowyer",
+        cardImage: "./images/portfolio/bowyer/bowyer-thumb.png",
+        techUsed:"HTML/CSS, WordPress, Google Analytics",
+        url:"https://www.drbowyer.com/",
         modalImages:[],
+        modalImageTemp:'./images/portfolio/bowyer/bowyer-modal.png',
+        modalSubtitle:"Your Cosmetic & Family Dentist in Vancouver, WA",
+        modalDescription:"While working under Explore Consulting, this client asked us to migrate their 20+ page website from an old hosting serving into WordPress.  I was responsible for in recreating the client's personal business page. I rebuilt the website from the ground up using WordPress, a few WordPress plugins, Google and Facebook analytics, and vanilla HTML/CSS. Dr. Bowyer saw significant growth in their business, and uses the original design to this day.",
         tags:[],
     },
+    {
+        companyTitle:"Earth Healer LLC",
+        cardImage: "./images/portfolio/earthHealer/earth-healer-thumb.png",
+        techUsed:"HTML/CSS, WordPress",
+        url:"https://earthhealerllc.com/",
+        modalImages:[],
+        modalImageTemp:'./images/portfolio/earthHealer/earthhealer_modal.jpeg',
+        modalSubtitle:"Garden Design and Classes",
+        modalDescription:" This is one of my first ever freelancing projects. I built this WordPress website with the help of a designer from Trendesigns, primarly using WordPress and HTML/CSS. Brian created the template, and I built the site using HTML/CSS and WordPress.",
+        tags:[],
+    },
+
     // {
     //     companyTitle:"China Med Device",
     //     cardImage: "",
@@ -64,6 +99,7 @@ const projectData = [
     //     modalImages:[],
     //     tags:[],
     // }
+
 ]
 
 
